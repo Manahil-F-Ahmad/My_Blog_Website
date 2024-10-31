e309691215a9eec1d160be88d583e4634852c486
Manahil Firdous <manahil.afirdous@gmail.com>
2024-10-31 21:13:14 +0000

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,28 @@
+// Functions for navigation to other tabs
+
+function homepage(){ //Function to go to Home page 
+    window.location.href = "homepage.html";
+}
+function contactpage(){ //Function to go to the contact form
+    window.open("https://forms.office.com/r/wwzzSCH9GS", "_blank");
+}
+function aboutmepage(){ //Funtion to go to the about me page
+    window.location.href = "aboutmepage.html";
+}
+
+function destinationpage(){ //Function to go to the destinations page
+    window.location.href = "destinationspage.html";
+}
+
+function photographypage(){ //Function to go to the photography page
+    window.location.href = "photographypage.html";
+}
+
+function foodpage(){ //Function to go to the food page
+    window.location.href = "foodpage.html";
+}
+
+function hackspage(){ //Function to go to the hacks page
+    window.location.href = "hackspage.html";
+}
+
